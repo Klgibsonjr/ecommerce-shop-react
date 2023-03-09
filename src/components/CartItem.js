bcd218461a341aeb434be4c46afdf8a4c1a39bcd
klgibsonjr <klgibsonjr@gmail.com>
2023-03-08 17:20:07 -0800

    made more progress on ecommerce tutorial

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // import Link
 import { Link } from 'react-router-dom';
@@ -6,11 +6,17 @@ import { Link } from 'react-router-dom';
 //import icons
 import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
 
+// import cart context
+import { CartContext } from '../contexts/CartContext';
+
 const CartItem = ({ item }) => {
+  const { removeFromCart, increaseAmount, decreaseAmount } =
+    useContext(CartContext);
+
   // deoconstruct item
   const { id, title, image, price, amount } = item;
   return (
-    <div className='flex'>
+    <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
       <div className='w-full min-h-[150px] flex items-center gap-x-4'>
         {/* image */}
         <Link to={`/product/${id}`}>
@@ -26,28 +32,41 @@ const CartItem = ({ item }) => {
               {title}
             </Link>
             {/* remove icon */}
-            <div className='text-xl cursor-pointer'>
+            <div
+              onClick={() => removeFromCart(id)}
+              className='text-xl cursor-pointer'>
               <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
             </div>
           </div>
-          <div className='bg-pink-200 flex gap-x-2 h-[36px]'>
+          <div className=' flex gap-x-2 h-[36px]'>
             {/* quantity */}
-            <div className='flex flex-1 max-w-[100px] bg-blue-400 items-center h-full border text-primary font-medium'>
+            <div className='flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium'>
               {/* minus itcon */}
-              <div className='flex-1'>
+              <div
+                onClick={() => decreaseAmount(id)}
+                className='flex-1 flex justify-center items-center cursor-pointer'>
                 <IoMdRemove />
               </div>
               {/* amount */}
-              <div>{amount}</div>
+              <div className='h-full flex justify-center items-center px-2'>
+                {amount}
+              </div>
               {/* plus icon */}
-              <div>
+              <div
+                onClick={() => increaseAmount(id)}
+                className='flex-1 h-full flex justify-center items-center cursor-pointer'>
                 <IoMdAdd />
               </div>
             </div>
             {/* item price */}
-            <div>item price</div>
+            <div className='flex-1 flex items-center justify-around'>
+              $ {price}
+            </div>
             {/* final price */}
-            <div>final price</div>
+            {/* mae the price at 2 decimals */}
+            <div className='flex-1 flex justify-end items-center text-primary font-medium'>{`$ ${parseFloat(
+              price * amount
+            ).toFixed(2)}`}</div>
           </div>
         </div>
       </div>
